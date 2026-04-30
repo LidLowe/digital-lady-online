269c63ec1123464a9a858ec94feb2522a666dad0
BaisultanRamazan <baisultan.ramazan@gmail.com>
2026-04-30 14:30:04 +0600

    updated team data

--- src/app/features/home/team/team.ts
+++ src/app/features/home/team/team.ts
@@ -4,19 +4,26 @@ import { TeamCard } from './team-card/team-card';
 @Component({
     selector: 'app-team',
     standalone: true,
-    imports: [
-        TeamCard
-    ],
+    imports: [TeamCard],
     templateUrl: './team.html',
     styleUrl: './team.css',
 })
-
 export class Team {
     items = [
-        { id: 1, name: "Template", role: "Template", url: "1.jpg" },
-        { id: 2, name: "Template", role: "Template", url: "2.jpg" },
-        { id: 3, name: "Template", role: "Template", url: "3.jpg" },
-        { id: 4, name: "Template", role: "Template", url: "4.jpg" },
-        { id: 5, name: "Template", role: "Template", url: "5.jpg" },
-    ]
+        {
+            id: 1,
+            name: 'Жумадилова\n' + 'Айнур\n' + 'Мырзахановна',
+            role: 'Проектный менеджер',
+            url: '1.jpg',
+        },
+        {
+            id: 2,
+            name: 'Нұрислам\n' + 'Еркінұлы',
+            role: 'Проектный менеджер',
+            url: '2.jpg',
+        },
+        { id: 4, name: 'Хамза Дәулетбек Ерғалиұлы', role: 'Руководитель отдела', url: '4.jpg' },
+        { id: 3, name: 'Байсұлтан Рамазан Аралбайұлы', role: 'Software Engineer', url: '3.jpg' },
+        { id: 5, name: 'Жасқайратұлы Марғұлан', role: 'Software Engineer', url: '5.jpg' },
+    ];
 }
